054104d00a79bab96c30c9af28bad03e6affe23a
kenia <keniayhdiaz@gmail.com>
2024-02-11 13:39:29 -0500

    a

--- main.py
+++ main.py
@@ -30,14 +30,20 @@ while fin == True:
         print("")
         print(" Bienvenido ", nameModCA)
         print("¿A qué área deseas ingresar: ")
-        print("1. Trainers \t2. Campers \t3. Reportes \t4. Regresar al incio")
+        print("1. Trainers \t2. Campers \t3. Rutas de entrenamiento  \t4. Reportes \t 5. Volver al menú principal")
         optionsAC = input(int(("Ingresa el número según el área a la cual deseas ingresar: ")))
         if optionsAC == "1":
             print("Has ingresado a la sección de Trainers.")
             print("¿Qué deseas hacer?")
             print("1. Crear nuevo Trainer \t2. Asignar horario \t3.Ver Trainers \t4.Regresar al inicio")
-            
-            
+        elif optionsAC == "2":
+            print("Has ingresado a la sección de Campers")
+            print("¿Qué deseas hacer?")
+            print("1. Matriculas \t2. Editar información \t3.Notas \t ")
+        elif optionsAC == "3":
+            print("Has ingresado a la sección de Rutas de entrenamiento")
+            print("¿Qué deseas hacer?")
+            print("1. Visualizar las Rutas \t 2. Crear nuevas rutas de entrenamiento")            
     elif rta == 2:
         print("-------------------------")
         print("-      ROL TRAINER      -")
