f037811cfa3c65e1828e28329539d897477d517a
kenia <keniayhdiaz@gmail.com>
2024-02-14 08:39:15 -0500

    Funciones

--- main.py
+++ main.py
@@ -22,16 +22,13 @@ while fin == True:
     print("")
     print("Seleciona el cargo al que perteneces:")
     print("1. Coordinador \t2. Trainer \t3.Salir")
-   
     rta = input("Ingresa el número según el rol: ")
     limpiarPantalla()
 
     if rta == '1':
         nameModCA = input("Ingresa tu nombre: ")
-
         limpiarPantalla()
         lejo.letrerito()
-
         print("")
         print(" Bienvenido ", nameModCA)
         print("¿A qué área deseas ingresar: ")
@@ -39,24 +36,27 @@ while fin == True:
         optionsAC = input("Ingresa el número según el área a la cual deseas ingresar: ")
         limpiarPantalla()
         if optionsAC == '1':
-            lejo.letrerito()
-            print("Has ingresado a la sección de Trainers.")
-            print("¿Qué deseas hacer?")
-            print("1. Crear nuevo Trainer \t2. Asignar horario \t3.Ver Trainers \t4.Regresar al inicio")
-            optionTr = input("Digite el número según la opción a la deseas ingresar: ")
-            if optionTr == '1':
-                mod.add_trainers()
-            elif optionTr == '2':
-                print("")
-            elif optionTr == '3':
-                print("")
-            elif optionTr == '4':
-                print("")
-        elif optionsAC == '2':
+            x = True
+            while x == True:
+                lejo.letrerito()
+                print("Has ingresado a la sección de Trainers.")
+                print("¿Qué deseas hacer?")
+                print("1. Crear nuevo Trainer \t2. Asignar horario \t3.Ver Trainers \n4. Volver al menú anterior")
+                optionTr = input("Digite el número según la opción a la deseas ingresar: ")
+                if optionTr == '1':
+                    mod.add_trainers()
+                elif optionTr == '2':
+                    print("")
+                elif optionTr == '3':
+                    print("")
+                elif optionTr == '4':
+                    x = False
+    elif optionsAC == '2':
+        while True:
             lejo.letrerito()
             print("Has ingresado a la sección de Campers")
             print("¿Qué deseas hacer?")
-            print("1. Incripciones \t2. Matriculas \n3. Editar información \t4.Notas \t ")
+            print("1. Incripciones \t2. Matriculas \n3. Editar información \t4.Notas \n5. Volver al menú anterior \t6. Salir ")
             optionsA = input("Digite el número según la opción a la que deseas ingresar: ")
             limpiarPantalla()
             if optionsA == '1':
@@ -68,16 +68,22 @@ while fin == True:
                 print("")
             elif optionsA == '4':
                 nt.add_notas()
-        elif optionsAC == '3':
-            print("Has ingresado a la sección de Rutas de entrenamiento")
-            print("¿Qué deseas hacer?")
-            print("1. Motrar Rutas \t 2. Crear rutas de entrenamiento")
-            optionR = input("Digite el número según la opción a la que deseas ingresar: ")
-            if optionR == '1':
-                mod.viewR()
-            elif optionR == '2':
-                print("Has ingresado a la sección de crear rutas.")
-                mod.newRuta()
+            elif optionsA == '5':
+                break
+            else:
+                if optionsA == '6':
+                    fin = False
+    elif optionsAC == '3':
+
+        print("Has ingresado a la sección de Rutas de entrenamiento")
+        print("¿Qué deseas hacer?")
+        print("1. Motrar Rutas \t 2. Crear rutas de entrenamiento")
+        optionR = input("Digite el número según la opción a la que deseas ingresar: ")
+        if optionR == '1':
+            mod.viewR()
+        elif optionR == '2':
+            print("Has ingresado a la sección de crear rutas.")
+            mod.newRuta()
     elif rta == '2':
         lejo.letrero()
         print("")
@@ -93,6 +99,7 @@ while fin == True:
             limpiarPantalla()
             print("Ingresa tu número de identificación.")
             trainerN = input("Número de identificación: ")
-    else:       
+    else:
+          
         if rta == '3':
             fin = False
